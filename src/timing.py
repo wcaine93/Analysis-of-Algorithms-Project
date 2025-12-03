a6b184810e7738b9987b0f08fb50533271766057
wcaine <wcaine93@gmail.com>
2025-12-02 23:39:22 -0500

    Creating timing.py
    Times execution of testing.py based on method from https://stackoverflow.com/a/53082285

---
+++ src/timing.py
@@ -0,0 +1,8 @@
+# Source - https://stackoverflow.com/a/53082285
+# Posted by Vedant Kandoi
+# Retrieved 2025-12-02, License - CC BY-SA 4.0
+
+import time
+st=time.time()
+import testing
+print("----%.10f----"%(time.time()-st))
