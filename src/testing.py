606a0270651909dffff91d56408a0e90bcb74f36
wcaine <wcaine93@gmail.com>
2025-12-02 23:38:30 -0500

    Update testing.py
    Implement steps 3-5, sorting and searching with mergesort and binary search.

--- src/testing.py
+++ src/testing.py
@@ -13,3 +13,21 @@ count = 0
 for number in numbers:
     if number == 53: count += 1
 print(count)
+
+# use mergesort and binary search to count
+# the number of occurrences of 53 in numbers
+import mergesort as ms
+import binary_search as bs
+ms.mergesort(numbers)
+pos = bs.binary_search(numbers, 53)
+# count 53's after and before found 53
+count = 0
+i = pos;
+while numbers[i] == 53:
+    count += 1
+    i += 1
+i = pos-1;
+while numbers[i] == 53:
+    count += 1
+    i -= 1
+print(count)
